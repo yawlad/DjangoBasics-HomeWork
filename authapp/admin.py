cb764f0456dcadacd7e77fbf1ccf5ae2d137bf47
yawlad <105426936+yawlad@users.noreply.github.com>
2022-10-12 12:06:06 +0300

    added admin stuff

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+
+from authapp import models as authapp_models
+
+
+@admin.register(authapp_models.CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ["id", "username", "email", "is_active", "date_joined"]
+    ordering = ["date_joined"]
